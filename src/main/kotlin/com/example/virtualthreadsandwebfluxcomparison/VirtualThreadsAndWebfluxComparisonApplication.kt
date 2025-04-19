e1e5ee354e48ff81fd0277ba37862ffc87a04d5f
Nhahan <kisy324@naver.com>
2025-04-20 00:40:59 +0900

    init

---
+++ src/main/kotlin/com/example/virtualthreadsandwebfluxcomparison/VirtualThreadsAndWebfluxComparisonApplication.kt
@@ -0,0 +1,11 @@
+package com.example.virtualthreadsandwebfluxcomparison
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VirtualThreadsAndWebfluxComparisonApplication
+
+fun main(args: Array<String>) {
+    runApplication<VirtualThreadsAndWebfluxComparisonApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/virtualthreadsandwebfluxcomparison/VirtualThreadsAndWebfluxComparisonApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.virtualthreadsandwebfluxcomparison
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VirtualThreadsAndWebfluxComparisonApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
